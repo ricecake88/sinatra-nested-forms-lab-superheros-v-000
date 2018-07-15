4668a7de8e4f1e5ad7f3f9fa4f57fa79c4c32945
Grace Shih <vocalise@gmail.com>
2018-07-15 22:14:32 +0000

    automatically backed up by learn

--- app/models/heroes.rb
+++ app/models/heroes.rb
@@ -1,5 +1,16 @@
 class Hero
   
+  attr_accessor :name, :power, :bio
+  
+  @HEROES = []
   def initialize(params)
-    @name = 
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @HEROES << self
+  end
+  
+  def self.all
+    @HEROES
+  end
 end
